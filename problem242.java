d9352e1395713ef3503c184d4c51967a0c9c153b
Kai Patel <106317141+KaiPatel22@users.noreply.github.com>
2025-05-30 15:51:35 +0100

    problem 242: valid anagrams completed, going to make it more efficient

---
+++ problem242.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+public class problem242 {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> hashmapS = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> hashmapT = new HashMap<Character, Integer>();
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for(char char1 : s.toCharArray()){
+            if(!hashmapS.containsKey(char1)){
+                hashmapS.put(char1, 1);
+            }else{
+                hashmapS.put(char1, (hashmapS.get(char1)+1));
+            }
+        }
+        
+        for(char char2 : t.toCharArray()){
+            if(!hashmapT.containsKey(char2)){
+                hashmapT.put(char2, 1);
+            }else{
+                hashmapT.put(char2, (hashmapT.get(char2)+1));
+            }
+        }
+        
+        if(hashmapS.equals(hashmapT)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
